8bc0ebcd7a238eda4296fb1dc8dbc1ce1f3cbfbf
arysaikyn <6183139aersi@gmail.com>
2024-02-03 22:54:57 -0500

    changed main

--- jodas.frontend/src/app/Components/Main.tsx
+++ jodas.frontend/src/app/Components/Main.tsx
@@ -1,10 +1,14 @@
 const Main = () => {
   return (
     <div className="h-full flex justify-between items-center">
-        <div className="h-full w-full flex justify-center items-center">
-            <div className="text-7xl">
+        <div className="h-full w-full flex flex-col justify-center items-center gap-10 m-10">
+            <div className="text-8xl">
                 Beyond Memory.
             </div>
+            <div className="text-4xl">
+                Go back and forth through TIME  with CapSoul!
+            </div>
+            <button className="btn btn-primary w-1/2 btn-lg">Register</button>
         </div>
         <video src="/rocket.mp4" muted autoPlay loop playsInline className="w-1/2 h-full object-cover"></video>
     </div>
