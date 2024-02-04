cb9161a562aa90aa1387aa2e5daa395ccb4fa10a
arysaikyn <6183139aersi@gmail.com>
2024-02-03 22:35:07 -0500

    test

--- jodas.frontend/src/app/Components/NavBar.tsx
+++ jodas.frontend/src/app/Components/NavBar.tsx
@@ -1,11 +1,11 @@
 const NavBar = () => {
   return (
-    <div className="navbar bg-main h-1/10">
+    <div className="navbar bg-main h-24 border-solid border-b white">
       <div className="flex-1 flex justify-start items-center">
         <img src="/logo.png" alt="logo" className="w-28 h-28"/>
         <a className="btn btn-ghost text-2xl">CapSoul</a>
       </div>
-      <div className="w-1/2 h-full">
+      <div className="h-full">
         <ul className="menu menu-horizontal px-1 flex flex-1 font-bold text-2xl h-full gap-20 justify-evenly">
           <li><a>About</a></li>
           <li><a>Get Started</a></li>
