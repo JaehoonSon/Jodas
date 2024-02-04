39550ba23fd67c6d36505e8fed00b738b1cf66b9
knuceles <son.jaehoon0826@gmail.com>
2024-02-04 12:03:30 -0500

    About page

---
+++ Jodas.API/Jodas.API/Services/HandleImage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Reflection;
+
+namespace Jodas.API.Services;
+
+public class HandleImage:IHandleImage
+{
+    private readonly string _pictureFolderPath;
+    public HandleImage()
+    {
+        _pictureFolderPath = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+    }
+
+    public async Task<byte[]> GetImage(int index)
+    {
+        string[] files = Directory.GetFiles(_pictureFolderPath);
+        if (files.Length >= index)
+        {
+            return new byte[0];
+        }
+        var picturePath = Path.Combine(_pictureFolderPath, files[index]);
+        return await File.ReadAllBytesAsync(picturePath);
+    }
+}
+
